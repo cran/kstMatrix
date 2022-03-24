41c50582a436c152e5ad14b9e77673ccc9121832
Cord Hockemeyer <cord.hockemeyer@uni-graz.at>
2022-03-24 14:40:06 +0000

    version 0.1-3

---
+++ R/kmcolors.R
@@ -0,0 +1,19 @@
+#' Determine a color vector based on probabilities
+#'
+#' \code{kmcolors} takes a probabilty vector and a color palette and
+#' creates a color vector to be used with \code{kmhasse}.
+#'
+#' @param prob Probability vector
+#' @param palette Color palette (default = cm.colors)
+#'
+#' @keywords math
+#'
+#' @importFrom grDevices cm.colors
+#'
+#' @export
+kmcolors <- function(prob, palette=cm.colors) {
+  if (!requireNamespace("igraph", quietly = TRUE)) {
+    stop(sprintf("Plotting requires package 'igraph'."))
+  }
+  palette(1001)[1001-1000*prob]
+}
