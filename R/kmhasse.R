0836f91e95a1035296c81fbf2e925417b7b9d85f
Cord Hockemeyer <cord.hockemeyer@uni-graz.at>
2022-10-24 13:12:44 +0000

    version 0.1-4

--- R/kmhasse.R
+++ R/kmhasse.R
@@ -14,49 +14,53 @@
 #'
 #' @export
 kmhasse <- function(struc, horizontal = TRUE, colors=NULL){
-  structure <- t(struc)
-  if (!requireNamespace("igraph", quietly = TRUE)) {
-    stop(sprintf("Plotting requires package 'igraph'."))
-  }
+structure <- t(struc)
+if (!requireNamespace("igraph", quietly = TRUE)) {
+stop(sprintf("Plotting requires package 'igraph'."))
+}
 
-  n <- ncol(structure)
-  b = diag(0,n)
+n <- ncol(structure)
+b = diag(0,n)
 
-  if (!(is.null(colors))) {
-    if (n != length(colors)) {
-      stop("Incompatible parameters!")
-    }
-  } else {
-    colors <- rep("#00dd00", n)
-  }
+if (!(is.null(colors))) {
+if (n != length(colors)) {
+stop("Incompatible parameters!")
+}
+} else {
+colors <- rep("#00dd00", n)
+}
 
-  for(i in 1:n){
-    for(j in 1:n){
-      if(sum(structure[,i]*structure[,j])==sum(structure[,i])) b[i,j]=1
-    }
-  }
-  diag(b)<-0
-  d <- b
-  for(i in 1:n){
-    for(j in c(1:n)[-i]){
-      if(b[j,i]==1) d[j,]=d[j,]*(1-b[i,])
-    }
-  }
-  ed <- NULL
-  for(i in 1:n) for(j in 1:n) if(d[i,j]==1) ed <- c(ed,i,j)
-  g1 <- igraph::graph( edges=ed, n=n, directed=TRUE )
-  l <- list("0")
-  for(i in 2:(n-1)) l[[i]] <- paste(c(c(letters[1:nrow(structure)])[structure[,i]*c(1:nrow(structure))]),collapse = '')
-  l[[n]] <- c("Q")
-  igraph::V(g1)$label <- l
-  coord = igraph::layout_with_sugiyama(g1)$layout
-  if (horizontal) { coord <- -coord[,c(2,1)] } else {coord <- -coord}
-  igraph::E(g1)$color <- 'black'
-  igraph::V(g1)$color <- colors
-  if (horizontal) {
-    igraph::plot.igraph(g1,layout=coord,vertex.frame.color="white",vertex.size=30, asp=.6)
-  } else {
-    igraph::plot.igraph(g1,layout=coord,vertex.frame.color="white",vertex.size=30, edge.arrow.mode = 0)
-  }
+for(i in 1:n){
+for(j in 1:n){
+if(sum(structure[,i]*structure[,j])==sum(structure[,i])) b[i,j]=1
+}
+}
+diag(b)<-0
+d <- b
+for(i in 1:n){
+for(j in c(1:n)[-i]){
+if(b[j,i]==1) d[j,]=d[j,]*(1-b[i,])
+}
+}
+ed <- NULL
+for(i in 1:n) for(j in 1:n) if(d[i,j]==1) ed <- c(ed,i,j)
+g1 <- igraph::graph( edges=ed, n=n, directed=TRUE )
+l <- list("0")
+for(i in 2:(n-1)) l[[i]] <- paste(c(c(letters[1:nrow(structure)])[structure[,i]*c(1:nrow(structure))]),collapse = '')
+l[[n]] <- c("Q")
+igraph::V(g1)$label <- l
+coord = igraph::layout_with_sugiyama(g1)$layout
+if (horizontal) {
+coord <- -coord[,c(2,1)]
+} else {
+coord <- -coord
+}
+igraph::E(g1)$color <- 'black'
+igraph::V(g1)$color <- colors
+if (horizontal) {
+igraph::plot.igraph(g1,layout=coord,vertex.frame.color="white",vertex.size=30, asp=.6)
+} else {
+igraph::plot.igraph(g1,layout=coord,vertex.frame.color="white",vertex.size=30, edge.arrow.mode = 0, ylim=c(-.8,.8))
+}
 }
 
--- R/kmneighbourhood.R
+++ R/kmneighbourhood.R
@@ -37,3 +37,47 @@ kmneighbourhood <- function(state, struct) {
   colnames(n) <- colnames(struct)
   n
 }
+
+
+
+#' Compute the n-neighbourhod of a state within a knowledge structure
+#'
+#' \code{kmnneighbourhood} computes the n-neighbourhood of a state
+#' within a knowledge structure, i.e. the family of all other
+#' states with a symmetric set difference maximal n.
+#'
+#' @param state Binary vector representing a knowledge state
+#' @param struct Binary matrix representing a knowledge structure
+#' @param distance Size of the n-neighbourhood
+#' @return Matrix containing the neighbouring states, one per row
+#'
+#' @examples
+#' kmnneighbourhood(c(1,1,0,0), xpl$space, 2)
+#'
+#' @keywords math
+#'
+#' @export
+kmnneighbourhood <- function(state, struct, distance) {
+  if (!inherits(struct, "matrix")) {
+    stop(sprintf("%s must be of class %s.", dQuote("struct"), dQuote("matrix")))
+  }
+  if (any(struct != 1*as.logical(struct))) {
+    stop(sprintf("%s must be a binary matrix.", dQuote("struct")))
+  }
+  if (length(state) != dim(struct)[2]) {
+    stop(sprintf("%s and %s don't match in size.", dQuote("state"), dQuote("struct")))
+  }
+  n <- matrix(
+    unlist(apply(struct, 1, function(x) {
+      d <- kmsetdistance(x, state)
+      if ((d >= 1) && (d <= distance))
+        return(x)
+      else
+        return(NULL)
+    })),
+    ncol = length(state),
+    byrow = TRUE
+  )
+  colnames(n) <- colnames(struct)
+  n
+}
