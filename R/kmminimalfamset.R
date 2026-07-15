fa9af406241daf47882508f3c13f5eed69ca0b73
Cord Hockemeyer <cord.hockemeyer@uni-graz.at>
2026-07-15 13:40:02 +0000

    version 2.3-4

--- R/kmminimalfamset.R
+++ R/kmminimalfamset.R
@@ -3,6 +3,14 @@
 #' @param x kmfamset
 #' @return kmfamset of minimal states
 #'
+#' \code{kmminimalfamset()} determines the sub family of minimal sets
+#' within a famset. If \code{x} is a knowledge structure, the result will
+#' be a family containing (only) the empty set because the empty set is a
+#' state in any knowledge structure and is its minimal state.
+#'
+#' The concept of the minimal state of a family of sets should not be confused
+#' with the concept of knowledge states minimal for some item.
+#'
 #' @family Utilities
 #'
 #' @examples
@@ -14,14 +22,15 @@
 #' @export
 kmminimalfamset <- function(x) {
   if (!(inherits(x, "kmfamset")))
-    stop("x must be of class 'kfamset'.")
+    stop("x must be of class 'kmfamset'.")
   nos <- dim(x)[1]
+  if (nos == 1) return(x)
   for (i in (1:(nos-1)))
     for (j in ((i+1):nos)) {
       if (all(x[i,] <= x[j,]))
-        return(kmminimalfamset(kmfamset(x[-j,])))
+        return(kmminimalfamset(kmfamset(matrix(x[-j,], ncol=dim(x)[2]))))
       else if (all(x[i,] >= x[j,]))
-        return(kmminimalfamset(kmfamset(x[-i,])))
+        return(kmminimalfamset(kmfamset(matrix(x[-i,], ncol=dim(x)[2]))))
     }
   x
 }
